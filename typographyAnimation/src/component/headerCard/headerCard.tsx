74fff66e520a9ae8e854eb19d8099429ef4c89e6
MichOchieng <MichOchieng@gmail.com>
2022-06-22 01:00:00 -0700

    Now spells name

--- typographyAnimation/src/component/headerCard/headerCard.tsx
+++ typographyAnimation/src/component/headerCard/headerCard.tsx
@@ -1,42 +1,33 @@
 import * as React from "react";
 import { Grid, Paper } from "@mui/material";
+import { Box } from "@mui/system";
+
+const name: string[] =   ['','','O','',
+                        'M','I','C','H',
+                          '','','H','',
+                          '','','I','',
+                          '','','E','',
+                          '','','N','',
+                          '','','G','']
 
 export const HeaderCard = () => {
-    return(
+    return (
         <Paper
             sx={{
                 border: "1px solid red",
                 textAlign: "center",
             }}
         >
-        <Grid
-            container
-        >
-            <Grid
-                item
-                xs={2}
+            <Grid 
+                container
             >
-                <h1>a</h1>
+                {name.map((_, index) => (
+                <Grid item xs={3} sm={3} md={3} key={index}>
+                    <h1>{name[index]}</h1>
+                </Grid>
+                ))}
             </Grid>
-            <Grid
-                item
-                xs={2}
-            >
-                <h1>b</h1>
-            </Grid>
-            <Grid
-                item
-                xs={2}
-            >
-                <h1>c</h1>
-            </Grid>
-            <Grid
-                item
-                xs={2}
-            >
-                <h1>d</h1>
-            </Grid>
-        </Grid>
+
         </Paper>
     )
 }
