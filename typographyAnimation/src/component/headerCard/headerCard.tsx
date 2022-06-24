320a5c01917a1502b8c030df2b4f4606eb3bc36c
MichOchieng <MichOchieng@gmail.com>
2022-06-24 01:32:52 -0700

    Actually works

--- typographyAnimation/src/component/headerCard/headerCard.tsx
+++ typographyAnimation/src/component/headerCard/headerCard.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Grid, Paper } from "@mui/material";
 import { Box } from "@mui/system";
+import { Transition, useSprings, animated, config, useTransition, useSpring } from "react-spring";
 
 const name: string[] =   ['','','O','',
                         'M','I','C','H',
@@ -11,23 +12,42 @@ const name: string[] =   ['','','O','',
                           '','','G','']
 
 export const HeaderCard = () => {
+
+    const base = {
+        config: { mass: 5, tension: 2000, friction: 200 },
+        to: { opacity: 1,transform: "translate3d(0, 0%, 0)" }, 
+        from: { opacity: 0,transform: "translate3d(0, -100%, 0)" },
+      }
+
+    const springs = useSprings(
+        name.length, name.map((t,i) => ({...base,delay: 100*i}))
+        )
+    
     return (
         <Paper
             sx={{
                 border: "1px solid red",
                 textAlign: "center",
+                maxWidth: "20%",
+                minWidth: "200px",
             }}
         >
             <Grid 
                 container
             >
-                {name.map((_, index) => (
-                <Grid item xs={3} sm={3} md={3} key={index}>
-                    <h1>{name[index]}</h1>
-                </Grid>
+                {springs.map((s, index) => (
+                    <Grid item xs={3} sm={3} md={3} key={index}>
+                        <animated.h1 
+                            key={`char${index}`}
+                            style={s}
+                        >
+                            {name[index]}
+                        </animated.h1>
+                    </Grid>
                 ))}
-            </Grid>
 
+            </Grid>
+ 
         </Paper>
     )
 }
