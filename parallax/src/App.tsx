b43ac9cd064a1fa38b8e9b664e9d197eb7461b86
MichOchieng <MichOchieng@gmail.com>
2022-06-27 13:08:05 -0700

    Moved parallax to new component

--- parallax/src/App.tsx
+++ parallax/src/App.tsx
@@ -1,68 +1,14 @@
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-import "../public/app.css";
+
 import * as React from "react";
-
-
-const moon = "../public/moon.jpg";
-const land = "../public/land.jpg";
-const asteroid  = "../public/asteroid.png";
+import { VerticalScroll } from "./components/verticalScroll";
+import { Grid } from "@mui/material";
 
 
 export default () => (
-  <div
-    className="main"
+  <Grid
+    container
   >
-    <Parallax 
-      pages={4} 
-    >
-      <ParallaxLayer
-        offset={0}
-        speed={1}
-        factor={2}
-        style={{
-          backgroundImage: `url(${moon})`,
-          backgroundSize: 'cover',
-        }}
-      >
-  
-      </ParallaxLayer>
-      
-      <ParallaxLayer
-        offset={2}
-        speed={1}
-        factor={4}
-        style={{
-          backgroundImage: `url(${land})`,
-          backgroundSize: 'cover',
-        }}
-      >
-  
-      </ParallaxLayer>
-      <ParallaxLayer
-        sticky={{ start: 0.9, end: 2.5 }}
-        style={{ textAlign: 'center' }}
-      >
-        <img src={asteroid}></img>
-      </ParallaxLayer>
-
-      <ParallaxLayer
-        offset={0.2}
-        speed={0.05}
-      >
-        <h2>
-          This is the first header
-        </h2>
-      </ParallaxLayer>
-
-      <ParallaxLayer
-        offset={3.2}
-        speed={2}
-      >
-        <h2>
-          This is the second header
-        </h2>
-      </ParallaxLayer>
-
-    </Parallax>
-  </div>
+    {/* <VerticalScroll/> */}
+    
+  </Grid>
 );
