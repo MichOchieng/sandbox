89456558eaebeb39da9dabb0c7e541b1d297cb6e
Mich Ochieng <michochieng@gmail.com>
2022-07-06 23:47:23 -0700

    First commit

---
+++ frameworks/tailwind/src/components/sidebar.tsx
@@ -0,0 +1,15 @@
+
+
+
+export default function SideBar () {
+    return(
+        <div
+            className=""
+        >
+            <i>A</i>
+            <i>B</i>
+            <i>C</i>
+            <i>D</i>
+        </div>
+    );
+};
