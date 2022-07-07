e9bc7d59d277ac6fd6445cd83e983e263303496c
Mich Ochieng <michochieng@gmail.com>
2022-07-07 12:14:23 -0700

    Added content

--- frameworks/tailwind/src/App.tsx
+++ frameworks/tailwind/src/App.tsx
@@ -1,10 +1,11 @@
 import SideBar from './components/sidebar';
-
+import Content from './components/content';
 
 export default function App() {
   return (
     <div className='flex'>
       <SideBar />
+      <Content />
     </div>
   )
 }
