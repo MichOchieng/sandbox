4c35feae65d2f40425cf518e6d3b153c1bc20fe8
Mich Ochieng <michochieng@gmail.com>
2022-07-07 12:12:49 -0700

    Added another div

--- frameworks/tailwind/src/components/sidebar.tsx
+++ frameworks/tailwind/src/components/sidebar.tsx
@@ -2,22 +2,34 @@ import { FaFire, FaPoo } from 'react-icons/fa';
 
 export default function SideBar () {
 
-    const SidebarIcon = ({ icon }:any) => (
-        <div className="sidebar-icon">
+    const SidebarIcon = ({ icon, text="Tooltip"}:any) => (
+        <div className="sidebar-icon group">
             {icon}
+            <span className='sidebar-tooltip group-hover:scale-100'>
+                {text}
+            </span>
         </div>
     );
 
     return(
-        <div
+        <>
+            <div
+                className="
+                fixed top-0 h-screen w-16 m-0 flex flex-col 
+                bg-primary text-white shadow-lg"
+            >
+                <SidebarIcon icon={<FaFire size='28'/>}/>
+                <SidebarIcon icon={<FaPoo  size='28'/>}/>
+                <SidebarIcon icon={<FaFire size='28'/>}/>
+                <SidebarIcon icon={<FaPoo  size='28'/>}/>
+            </div>
+            <div
             className="
-            fixed top-0 h-screen w-16 m-0 flex flex-col 
-            bg-primary text-white shadow-lg"
-        >
-            <SidebarIcon icon={<FaFire size='28'/>}/>
-            <SidebarIcon icon={<FaPoo  size='28'/>}/>
-            <SidebarIcon icon={<FaFire size='28'/>}/>
-            <SidebarIcon icon={<FaPoo  size='28'/>}/>
-        </div>
+            h-screen w-32 flex-col 
+            bg-secondary text-white shadow-lg"
+            >
+                
+            </div>
+        </>
     );
 };
