c46e0e9f19312eb83d8cdd27f903e0f3f0a63ea7
MichOchieng <MichOchieng@gmail.com>
2022-07-07 14:06:34 -0700

    Changed bg colour

--- frameworks/tailwind/src/components/content.tsx
+++ frameworks/tailwind/src/components/content.tsx
@@ -1,7 +1,7 @@
 const Content = () => {
     return(
         <div
-            className="flex h-screen w-screen bg-gray-800"
+            className="flex h-screen w-screen bg-gray-600"
         >
 
         </div>
