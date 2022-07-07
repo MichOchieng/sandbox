e1aee6f8ee1761da1e6361a31e039f30277f7fbb
Mich Ochieng <michochieng@gmail.com>
2022-07-07 00:03:35 -0700

    Added sidebar icon

--- frameworks/tailwind/src/components/sidebar.tsx
+++ frameworks/tailwind/src/components/sidebar.tsx
@@ -1,15 +1,23 @@
-
-
+import { FaFire, FaPoo } from 'react-icons/fa';
 
 export default function SideBar () {
+
+    const SidebarIcon = ({ icon }:any) => (
+        <div className="sidebar-icon">
+            {icon}
+        </div>
+    );
+
     return(
         <div
-            className=""
+            className="
+            fixed top-0 h-screen w-16 m-0 flex flex-col 
+            bg-primary text-white shadow-lg"
         >
-            <i>A</i>
-            <i>B</i>
-            <i>C</i>
-            <i>D</i>
+            <SidebarIcon icon={<FaFire size='28'/>}/>
+            <SidebarIcon icon={<FaPoo  size='28'/>}/>
+            <SidebarIcon icon={<FaFire size='28'/>}/>
+            <SidebarIcon icon={<FaPoo  size='28'/>}/>
         </div>
     );
 };
