5bdd6cacb162c20ddad7241ec547755025e62341
Mich Ochieng <michochieng@gmail.com>
2022-07-27 13:28:34 -0700

    Changes

--- framer/src/App.tsx
+++ framer/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { ExpandingGallery } from './components/expandingGallery';
 import { LoadingPage } from './components/loadingPage';
 import { TextBox } from './components/textBox';
+import { CardData } from './data/cardData';
 function App() {
   return (
     <div className='flex h-screen w-screen items-center justify-center border border-black'>
       {/* <TextBox/> */}
-      <LoadingPage/>
+      {/* <LoadingPage/> */}
+      <ExpandingGallery data={CardData}/>
     </div>
   );
 }
