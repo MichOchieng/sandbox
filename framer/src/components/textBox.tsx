ace86af9bbb654ee9e9758b7c754872196d81e91
Mich Ochieng <michochieng@gmail.com>
2022-07-27 02:21:18 -0700

    First commit

---
+++ framer/src/components/textBox.tsx
@@ -0,0 +1,14 @@
+import { motion } from 'framer-motion';
+
+export const TextBox = () => {
+    return (
+        <div className="
+        flex w-1/2 h-1/4 overflow-hidden
+        text-center
+        border border-black rounded-lg p-4
+        "
+        >
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ad ipsum saepe autem sapiente id rerum cupiditate necessitatibus facere. Porro expedita voluptas ducimus consequuntur ullam illum tempore, ab reprehenderit quidem.</p>
+        </div>
+    )
+}
