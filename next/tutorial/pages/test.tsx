7f57eb8adc053e112cdd202ac058d8c61779efa9
MichOchieng <MichOchieng@gmail.com>
2022-08-23 13:58:00 -0700

    First commit

---
+++ next/tutorial/pages/test.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Test: NextPage = () => {
+    return (
+      <div className={""}>
+        <h1>Test</h1>
+      </div>
+    )
+  }
+  
+  export default Test
