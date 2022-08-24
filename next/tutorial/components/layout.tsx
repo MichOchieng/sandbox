1561f41c3db51e6cd7202c134f6915e4360d7e17
Mich Ochieng <michochieng@gmail.com>
2022-08-23 21:41:59 -0700

    First commit

---
+++ next/tutorial/components/layout.tsx
@@ -0,0 +1,13 @@
+import { Children } from "react"
+import { Navbar } from "./navbar"
+
+const Layout = ({ children }:any) => {
+    return(
+        <>
+            <Navbar/>
+        <   main>{children}</main>
+        </>
+    )
+}
+
+export default Layout
---
+++ next/tutorial/components/navbar.tsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+    return(
+        <div
+            className="navBar"
+        >
+            <ul>
+                <li>THis</li>
+                <li>Is</li>
+                <li>A</li>
+                <li>Navbar</li>
+            </ul>
+        </div>
+    )
+}
