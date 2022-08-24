af2127da2160f04ee6e26429930c2447086d5fcc
Mich Ochieng <michochieng@gmail.com>
2022-08-23 21:42:48 -0700

    Added tailwind and removed old css

--- next/tutorial/pages/index.tsx
+++ next/tutorial/pages/index.tsx
@@ -2,11 +2,10 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="">
       <h1>Home</h1>
       <Link href="/profile">Profile</Link>
     </div>
