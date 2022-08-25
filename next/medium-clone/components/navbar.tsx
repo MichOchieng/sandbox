a4a4bc1b9e2b93cd3730ebfa8dc9dd186b53b7a3
Mich Ochieng <michochieng@gmail.com>
2022-08-25 13:18:38 -0700

    Added buttons

--- next/medium-clone/components/navbar.tsx
+++ next/medium-clone/components/navbar.tsx
@@ -5,7 +5,30 @@ export const Navbar = () => {
         <div
             className="navBar"
         >
-            <h1>test</h1>
+            {/* Left Nav links */}
+            <div
+                className="navBar-container"
+            >
+                {/* Logo/Icon */}
+                <h1 className="text-white font-bold text-xl tracking-widest mx-2">Medium</h1>
+                <h1 className="navBar-button">About</h1>
+                <h1 className="navBar-button">Contact</h1>
+                <h1 className="
+                navBar-button border border-black smoothed
+                rounded-3xl p-2 bg-black hover:scale-105
+                ">Follow</h1>
+            </div>
+            {/* Right nav links */}
+            <div
+                className="navBar-container"
+            >
+                <h1 className="navBar-button">Sign In</h1>
+                <h1 className="
+                navBar-button border-black border
+                rounded-3xl p-2 hover:scale-105 smoothed
+                ">Get Started</h1>
+            </div>
+
         </div>
     )
 }
