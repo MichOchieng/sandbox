1b337409bdc238b4dfa677adab0841686e4d3d76
MichOchieng <MichOchieng@gmail.com>
2022-08-25 11:44:53 -0700

    First commit

---
+++ next/medium-clone/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <h1>
+        aaaa
+      </h1>
+    </div>
+  )
+}
+
+export default Home
