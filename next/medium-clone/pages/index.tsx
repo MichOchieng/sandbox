94a7abe8a410026906c932df4c3f67a1466c78c1
Mich Ochieng <michochieng@gmail.com>
2022-08-26 09:26:37 -0700

    Minor change

--- next/medium-clone/pages/index.tsx
+++ next/medium-clone/pages/index.tsx
@@ -2,10 +2,13 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
-      <h1>
-        aaaa
-      </h1>
+    <div className="h-full w-full">
+      {/* Header */}
+      <div
+        className="aid h-2/6 w-full"
+      >
+        <h1>test</h1>
+      </div>
     </div>
   )
 }
