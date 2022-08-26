98a758caf5ad7c692f89b7535c1c232ccff0f458
Mich Ochieng <michochieng@gmail.com>
2022-08-26 09:26:56 -0700

    Added layout

--- next/medium-clone/pages/_app.tsx
+++ next/medium-clone/pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Layout } from '../components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Layout children={
+    <Component {...pageProps} />
+  }/>
 }
 
 export default MyApp
