6798c0943b053267f55d845ad85a340d6d3d76fc
Mich Ochieng <michochieng@gmail.com>
2022-08-26 09:26:21 -0700

    Added links

--- next/medium-clone/components/navbar.tsx
+++ next/medium-clone/components/navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 
 
 export const Navbar = () => {
@@ -5,27 +6,34 @@ export const Navbar = () => {
         <div
             className="navBar"
         >
-            {/* Left Nav links */}
+            {/* Logo/Icon */}
+            <Link href="/">
+                    <img 
+                        className="object-contain w-3/6 md:w-1/4 smoothed cursor-pointer"
+                        src="https://links.papareact.com/yvf"
+                    />
+            </Link>
+            {/* Nav links */}
             <div
                 className="navBar-container"
             >
-                {/* Logo/Icon */}
-                <h1 className="text-white font-bold text-xl tracking-widest mx-2">Medium</h1>
-                <h1 className="navBar-button">About</h1>
-                <h1 className="navBar-button">Contact</h1>
+                <Link 
+                    href="/about"
+                >
+                    <h1 className="navBar-button hidden md:flex">About</h1>
+                </Link>
+                <Link href="/contact">
+                    <h1 className="navBar-button hidden md:flex">Contact</h1>
+                </Link>
+            
                 <h1 className="
-                navBar-button border border-black smoothed
-                rounded-3xl p-2 bg-black hover:scale-105
+                navBar-button hidden md:flex
                 ">Follow</h1>
-            </div>
-            {/* Right nav links */}
-            <div
-                className="navBar-container"
-            >
-                <h1 className="navBar-button">Sign In</h1>
+                <h1 className="navBar-button text-center hidden md:flex">Sign In</h1>
                 <h1 className="
-                navBar-button border-black border
-                rounded-3xl p-2 hover:scale-105 smoothed
+                navBar-button border-slate-400 border bg-black items-center
+                rounded-3xl p-2 hover:scale-105 smoothed text-center 
+                w-1/2 h-1/3 md:w-1/4 flex justify-center
                 ">Get Started</h1>
             </div>
 
