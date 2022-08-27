fe64487d15b947f7e6a3c4209a119c2d1861da80
MichOchieng <MichOchieng@gmail.com>
2022-08-26 18:17:29 -0700

    Updated image builder

--- next/medium-clone/sanity.js
+++ next/medium-clone/sanity.js
@@ -1,8 +1,9 @@
 import {
-    createImageUrlBuilder,
     createCurrentUserHook,
     createClient,
 } from "next-sanity"
+
+import createImageUrlBuilder from "@sanity/image-url"
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
@@ -12,5 +13,5 @@ export const config = {
 };
 
 export const sanityClient = createClient(config);
-export const urlFor = (source) => createImageUrlBuilder(config).image(source);
+export function urlFor (source) {return createImageUrlBuilder(config).image(source)};
 export const useCurrentUser = createCurrentUserHook(config);
