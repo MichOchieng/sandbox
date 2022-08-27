63e59f756e0fb642511e20a0fba796859dcdecf7
MichOchieng <MichOchieng@gmail.com>
2022-08-26 17:11:01 -0700

    Added Props and ssr to get sanity posts

--- next/medium-clone/pages/index.tsx
+++ next/medium-clone/pages/index.tsx
@@ -1,15 +1,49 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import { Header } from '../components/header'
 import { Posts } from '../components/posts'
+import {sanityClient, urlFor} from "../sanity"
+import { Post } from '../typings'
 
-const Home: NextPage = () => {
+
+interface Props {
+  posts: [Post]
+}
+
+const Home: NextPage<Props> = ( {posts}:Props ) => {
+  
   return (
-    <div className="h-full w-full">
+    <div className="h-fit w-full">
+      <Head>
+        <title>Medium Clone</title>
+      </Head>
       {/* Header */}
       <Header/>
-      <Posts/>
+      <Posts posts={posts}/>
     </div>
   )
+}
+
+export const getServerSideProps = async ({}) =>{
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author => {
+        name,
+        image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    }
+  }
 }
 
 export default Home
