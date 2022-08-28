ab9d7d5e667842c341e76a4ecc2ba38589bf6640
MichOchieng <MichOchieng@gmail.com>
2022-08-28 14:22:10 -0700

    First commit

---
+++ next/medium-clone/pages/post/[slug].tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import { sanityClient, urlFor } from "../../sanity";
+
+const Post: NextPage = () => {
+    return(
+        <div>
+            <h1>aaa</h1>
+        </div>
+    )
+}
+
+export const getStaticPaths = async () => {
+
+}
+
+export default Post
