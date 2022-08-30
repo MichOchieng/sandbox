306a368c37dd3ae196484bd16d57ed7ae1416f9d
Mich Ochieng <michochieng@gmail.com>
2022-08-29 23:04:34 -0700

    First commig

---
+++ next/loading-screen/components/dummy.tsx
@@ -0,0 +1,14 @@
+
+
+export const Dummy = () => {
+    return(
+        <div className="h-fit w-full"> 
+            <div className="h-96 w-full aid">a</div>
+            <div className="h-96 w-full aid">b</div>
+            <div className="h-96 w-full aid">c</div>
+            <div className="h-96 w-full aid">d</div>
+            <div className="h-96 w-full aid">e</div>
+            <div className="h-96 w-full aid">f</div>
+        </div>
+    )
+}
