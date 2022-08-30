8c8adf9abb03df9bc3e82720312d1cef857d59ab
Mich Ochieng <michochieng@gmail.com>
2022-08-29 23:05:56 -0700

    minor updates

--- next/loading-screen/pages/index.tsx
+++ next/loading-screen/pages/index.tsx
@@ -1,11 +1,30 @@
 import type { NextPage } from 'next'
+import { Dummy } from '../components/dummy'
 
 const Home: NextPage = () => {
   return (
     <div
-      className='home h-screen w-screen flex items-center justify-center'
+      className='
+      home h-screen w-screen flex flex-col 
+      overflow-scroll items-center
+      '
     >
       <h1 className='text-8xl font-bold'>home</h1>
+      <button
+        onClick={
+          () => {
+            localStorage.setItem('loading','true')
+          }
+        }
+      >loading thing true</button>
+      <button
+        onClick={
+          () => {
+            localStorage.setItem('loading','false')
+          }
+        }
+      >loading thing false</button>
+      <Dummy/>
     </div>
   )
 }
