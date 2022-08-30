91794dd0214f27484cd1d5f6c13ec8c6c4392f69
MichOchieng <MichOchieng@gmail.com>
2022-08-30 13:16:21 -0700

    Added useloadingstate function

--- next/loading-screen/components/layout.tsx
+++ next/loading-screen/components/layout.tsx
@@ -1,13 +1,33 @@
 import { useEffect, useState } from "react"
 import { PageLoad } from "./pageload"
 
+function useLoadingState (initialVal:string, key:string){
+    const [value,setValue] = useState(initialVal)
+
+    useEffect(() => {
+        const loadingValue = window.localStorage.getItem(key)
+
+        if(loadingValue !== null) {
+            setValue(JSON.parse(loadingValue))
+        }
+    },[key])
+
+    useEffect(() => {
+        window.localStorage.setItem(key, JSON.stringify(value))
+    },[key, value])
+
+    return [value,setValue]
+}
+
 export const Layout = ({children}:any) => {
 
-    const [loading,setLoading] = useState("true")
+    const [loading,setLoading] = useLoadingState("true","loading")
 
     useEffect(() => {
         setTimeout(() => {
-            setLoading("false")
+            if (typeof setLoading == "function") {
+                setLoading("false")
+            }
         }, 3000)
     },[])
 
@@ -16,7 +36,7 @@ export const Layout = ({children}:any) => {
             className="w-screen relative aid h-screen flex flex-col mx-auto items-center"
         >
             {
-                loading && <PageLoad/>
+                (loading === "true") && <PageLoad/>
             }
             {children}
         </div>
