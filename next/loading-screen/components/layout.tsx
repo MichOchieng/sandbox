5596ef39eeae8367b13b5b4546fb86ab874ad3f0
Mich Ochieng <michochieng@gmail.com>
2022-08-29 23:04:50 -0700

    Added loading page

--- next/loading-screen/components/layout.tsx
+++ next/loading-screen/components/layout.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react"
 import { PageLoad } from "./pageload"
 
 export const Layout = ({children}:any) => {
+
+    const [loading,setLoading] = useState("true")
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading("false")
+        }, 3000)
+    },[])
+
     return(
         <div
             className="w-screen relative aid h-screen flex flex-col mx-auto items-center"
         >
-            <PageLoad/>
+            {
+                loading && <PageLoad/>
+            }
             {children}
         </div>
     )
