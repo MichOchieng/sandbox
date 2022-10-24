ddfa81233df2b8b08aeb9338eb78d8c860e7e2fa
MichOchieng <MichOchieng@gmail.com>
2022-10-23 23:51:24 -0700

    Added dark light toggle

--- day-night/pages/index.tsx
+++ day-night/pages/index.tsx
@@ -1,30 +1,39 @@
 import type { NextPage } from 'next'
-import { createContext, useState } from 'react'
+import Head from 'next/head';
 
-type ThemeName = 'light' | 'dark' | string
-type ThemeContextType = {
-  theme: ThemeName,
-  setTheme: (name: ThemeName) => void
+
+const checkTheme = () => {
+  if (localStorage.getItem('color-theme')) {
+    if (localStorage.getItem('color-theme') === 'light') {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+    }
+
+// if NOT set via local storage previously
+} else {
+    if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+    } else {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+    }
 }
-
-const getInitialTheme = () => {
-  if (typeof window !== 'undefined' && window.localStorage) {
-    const preferences = window.localStorage.getItem('theme');
-    return preferences
-  }
-  return 'light'
 }
-
-const ThemeContext = createContext<ThemeContextType>({} as ThemeContextType)
 
 const Home: NextPage = () => {
 
   return (
     <div
-      className='w-screen h-screen flex flex-col items-center justify-center bg-white dark:bg-slate-900'
+      className='smoothed w-screen h-screen flex flex-col items-center justify-center bg-white dark:bg-slate-900'
     >
-      <div className='w-3/5 h-96 border border-black dark:border-slate-500 flex items-center justify-center bg-white dark:bg-slate-700'>
-        <button>Toggle</button>
+      <div className='smoothed w-3/5 h-96 border border-black dark:border-slate-500 flex items-center justify-center bg-white dark:bg-slate-700'>
+        <button
+          onClick={() => checkTheme()}
+        >Toggle</button>
       </div>
     </div>
   )
