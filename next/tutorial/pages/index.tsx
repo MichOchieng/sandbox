7585bc1012f58af30a4c4e6012d24b223cf4cf5e
MichOchieng <MichOchieng@gmail.com>
2023-02-16 16:53:02 -0800

    Removed

--- next/tutorial/pages/index.tsx
+++ next/tutorial/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import { Button } from '@michochieng/momo-toolbox'
 
 const Home: NextPage = () => {
   return (
@@ -7,6 +8,8 @@ const Home: NextPage = () => {
       <Link href="/">Home</Link>
       <Link href="/profile">Profile</Link>
       <Link href="/coins">Coins</Link>
+      <Button text='momo' outlined={false} classname=''/>
+      <button className='button-outlined mx-auto my-2'>button</button>
     </div>
   )
 }
