822c43e807a018a9f076dc16a15e9eb568c40bf4
MichOchieng <MichOchieng@gmail.com>
2023-02-16 18:04:42 -0800

    Added basic logic

--- canvas/components/pointerBlur.tsx
+++ canvas/components/pointerBlur.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const PointerBlur = () => {
+  React.useEffect(() => {
+    const blob = document.getElementById('blob')
+
+    document.body.onpointermove = event => {
+        const {clientX, clientY} = event
+        blob!.style.left = `${clientX}px`;
+        blob!.style.top = `${clientY}px`;
+    }
+  })
+
   return (
-    <div>PointerBlur</div>
+    <div id='blob' className='blob'></div>
   )
 }
 
