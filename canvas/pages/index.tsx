c57b4458bac15d64e8128047f76b2792bcacafe4
MichOchieng <MichOchieng@gmail.com>
2023-02-19 22:43:24 -0800

    Minor change

--- canvas/pages/index.tsx
+++ canvas/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='w-screen h-screen flex items-center justify-center border'>
+      <main className='w-screen h-screen flex items-center justify-center border overflow-hidden'>
         <PointerBlur/>
       </main>
     </>
