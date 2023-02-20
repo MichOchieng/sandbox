4ac1f62b242ccab9cb2fe3a569de73623f6c5fb6
MichOchieng <MichOchieng@gmail.com>
2023-02-20 13:58:23 -0800

    Added code for animation

--- canvas/components/exposiveHeader.tsx
+++ canvas/components/exposiveHeader.tsx
@@ -1,8 +1,41 @@
 import React from 'react'
+interface HeaderProps {
+    title: string
+}
 
-const ExplosiveHeader = () => {
+const ExplosiveHeader = ( {title}:HeaderProps ) => {
+
+    React.useEffect(() => {
+        let isMounted = true; // add a variable to track whether the component is mounted
+        
+        const header = (id: string) => {
+          const element = document.getElementById(id);
+          const text = title.split("");
+          element!.innerText = ""
+          text.forEach((letter) => {
+            const span = document.createElement("span");
+            span.className = "letter";
+            span.innerText = letter;
+            element!.appendChild(span);
+          });
+        };
+      
+        if (isMounted) { // check if the component is mounted before running the effect
+          header("explosiveHeader");
+          console.log('Test');
+          
+        }
+      
+        return () => {
+          isMounted = false; // update the variable when the component is unmounted
+        };
+      }, []);
+
+
   return (
-    <div>ExplosiveHeader</div>
+    <header id='explosiveHeader' className='letterContainer'>
+        
+    </header>
   )
 }
 
