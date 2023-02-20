70757aab6dbc5ea9251a51b4aab2783d834f5a32
MichOchieng <MichOchieng@gmail.com>
2023-02-19 22:43:13 -0800

    Added background blur and lagging effect to blob

--- canvas/components/pointerBlur.tsx
+++ canvas/components/pointerBlur.tsx
@@ -6,13 +6,19 @@ const PointerBlur = () => {
 
     document.body.onpointermove = event => {
         const {clientX, clientY} = event
-        blob!.style.left = `${clientX}px`;
-        blob!.style.top = `${clientY}px`;
+        blob!.animate({
+            left: `${clientX}px`,
+            top: `${clientY}px`
+        },{ duration: 3000, fill:'forwards'})
+
     }
   })
 
   return (
-    <div id='blob' className='blob'></div>
+    <>
+        <div id='blob' className='blob'></div>
+        <div id='blur' className='blur'></div>
+    </>
   )
 }
 
