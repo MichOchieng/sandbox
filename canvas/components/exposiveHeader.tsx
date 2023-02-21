67b9beb490843f9bb46bdede1752d949df78a64d
MichOchieng <MichOchieng@gmail.com>
2023-02-20 17:05:45 -0800

    Added support for floating effect

--- canvas/components/exposiveHeader.tsx
+++ canvas/components/exposiveHeader.tsx
@@ -7,16 +7,37 @@ const ExplosiveHeader = ( {title}:HeaderProps ) => {
 
     React.useEffect(() => {
         let isMounted = true; // add a variable to track whether the component is mounted
+
+        const rand = (min:number, max:number) => Math.floor(Math.random() * (max - min + 1)) + min;
         
         const header = (id: string) => {
           const element = document.getElementById(id);
           const text = title.split("");
           element!.innerText = ""
-          text.forEach((letter) => {
-            const span = document.createElement("span");
-            span.className = "letter";
-            span.innerText = letter;
-            element!.appendChild(span);
+
+          text.forEach((letter,index) => {
+            // This span will have the explosion effect and hold the innerSpan element
+            const outerSpan = document.createElement("span");
+            outerSpan.className = "outer";
+            
+            // This span will have the floating effect and hold a letter
+            const innerSpan = document.createElement("span");
+            innerSpan.className = "inner";
+            innerSpan.style.animationDelay = `${rand(-5000, 0)}ms`;
+
+            // A span containing a letter from the input text
+            const letterSpan = document.createElement("span");
+            letterSpan.className = "letter";
+            letterSpan.innerText = letter;
+
+            letterSpan.style.animationDelay = `${index * 1000 }ms`; // Gives each letter its own hovering bounce effect thingy
+
+            // Glue everything together
+            innerSpan.appendChild(letterSpan)
+
+            outerSpan.appendChild(innerSpan)
+
+            element!.appendChild(outerSpan);
           });
         };
       
@@ -33,7 +54,7 @@ const ExplosiveHeader = ( {title}:HeaderProps ) => {
 
 
   return (
-    <header id='explosiveHeader' className='letterContainer'>
+    <header id='explosiveHeader' className='headerContainer'>
         
     </header>
   )
