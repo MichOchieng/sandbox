501ac449b3b1bc6f00f698b33a0f52ce49a44d52
MichOchieng <MichOchieng@gmail.com>
2023-04-06 00:37:56 -0700

    Added links and changed metadata

--- thirteen/app/page.tsx
+++ thirteen/app/page.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link"
 import React from "react"
 
 export default function Home() {
   return (
     <main className="">
-      <h1 className="font-bold">test</h1>
+      <h1 className="font-bold">Home Page</h1>
+      <ul>
+        <li>
+          <Link href={'/'}>Home</Link>
+        </li>
+        <li>
+          <Link href={'/about'}>About</Link>
+        </li>
+        <li>
+          <Link href={'/about/team'}>About/Team</Link>
+        </li>
+      </ul>
     </main>
   )
 }
