a9ef0c9b19d5e5286ad34e0911e34321ee4b730e
MichOchieng <MichOchieng@gmail.com>
2023-04-06 00:31:19 -0700

    First comit

---
+++ thirteen/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime maiores praesentium illo dolore consequuntur soluta vitae consectetur voluptatum distinctio placeat impedit magni id numquam aut repudiandae earum, quod porro dicta.</p>
+    </div>
+
+  )
+}
+
+export default About
